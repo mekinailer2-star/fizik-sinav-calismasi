9d982955c81e8dc5aa8821abb13da2feb03c5952
mekinailer2-star <mekinailer2@gmail.com>
2026-04-05 21:46:51 +0300

    Bump SW cache v2, switch to network-first strategy

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'fizik-sinav-v1';
+const CACHE_NAME = 'fizik-sinav-v2';
 const ASSETS = [
     './',
     './index.html',
@@ -27,6 +27,12 @@ self.addEventListener('activate', event => {
 
 self.addEventListener('fetch', event => {
     event.respondWith(
-        caches.match(event.request).then(response => response || fetch(event.request))
+        fetch(event.request)
+            .then(response => {
+                const clone = response.clone();
+                caches.open(CACHE_NAME).then(cache => cache.put(event.request, clone));
+                return response;
+            })
+            .catch(() => caches.match(event.request))
     );
 });
